f03f587b0d16dfa1d3e4e77ac8ccbbfbffbe88d4
jake <32205542+wakejalker@users.noreply.github.com>
2024-06-13 02:11:32 -0500

    Thymeleaf Views Exercises

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,14 +1,14 @@
 package org.launchcode.codingevents.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Chris Bay
@@ -17,28 +17,30 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
+    private static final Map<String, String> events = new HashMap<>();
 
+    static {
+        events.put("Code and Coffee", "Join us for coding and coffee every Saturday morning.");
+        events.put("Tech Talks", "Learn about the latest in technology from industry experts.");
+        events.put("Hackathon", "Compete with other developers in a 24-hour coding challenge.");
+    }
 
     @GetMapping
     public String displayAllEvents(Model model) {
-       model.addAttribute("events", events);
+        model.addAttribute("events", events);
         return "events/index";
     }
 
     // lives at events/create
     @GetMapping("create")
     public String renderCreateEventForm() {
-
         return "events/create";
     }
 
     // lives at /events/create
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName) {
-        events.add(eventName);
+    public String createEvent(@RequestParam String eventName, @RequestParam String eventDescription) {
+        events.put(eventName, eventDescription);
         return "redirect:/events";
     }
-
-
 }
